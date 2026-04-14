ad00d41088bcce029a098ad2d7fdd913d69cece4
KiseraTimon <timonmarick@gmail.com>
2026-04-14 22:16:00 +0300

    Main

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
 #include "include/System/SimulationEngine.h"
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 int main() {
-    std::cout << "--- COFFEE FACTORY SIMULATION ---\n";
+    std::cout << "========================================\n";
+    std::cout << "   COFFEE FACTORY SIMULATION V1.0       \n";
+    std::cout << "========================================\n";
 
     FactorySim::SimulationEngine engine;
 
-    // Building the machines and workers
     engine.initializeFactory();
 
-    // Starting the simulation loop
-    engine.run();
+    // Running the simulation engine in a background thread to stop it safely in future
+    std::thread engine_thread(&FactorySim::SimulationEngine::run, &engine);
 
+    // Running the factory for 30 seconds
+    std::this_thread::sleep_for(std::chrono::seconds(30));
+
+    std::cout << "\n[SYS] Shutting down factory pipeline...\n";
+    engine.stop();
+    engine_thread.join();
+
+    std::cout << "Simulation Complete. Exiting.\n";
     return 0;
 }
