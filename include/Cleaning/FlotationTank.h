65a264cd734d78360ded1149ea74023d379d1ef7
KiseraTimon <timonmarick@gmail.com>
2026-04-14 19:19:32 +0300

    Flotation Machine Class Declaration

---
+++ include/Cleaning/FlotationTank.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <setjmp.h>
+
+#include "../Core/Machine.h"
+
+namespace FactorySim {
+    class FlotationTank : public Machine {
+    private:
+        float capacity; // Litres
+        float water_temp;
+        float float_removal_rate;
+        float water_level;  // per kgs/litres
+        float cycle_duration;   // mins
+
+    public:
+        FlotationTank(
+            std::string id,
+            float capacity,
+            float water_temp,
+            float float_removal_rate,
+            float water_level,
+            float cycle_duration);
+
+        float calculateDefectsRemoved(float contamination) const;
+
+        // Override Method
+        void process(Batch &batch, float delta) override;
+        void onBreakdown() override;
+
+    };
+}
