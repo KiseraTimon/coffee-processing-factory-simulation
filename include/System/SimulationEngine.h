bb7abdb03b9e581a59716e42a950d798c73880c9
KiseraTimon <timonmarick@gmail.com>
2026-04-14 22:05:24 +0300

    Revised Simulation Engine Class Declarations

--- include/System/SimulationEngine.h
+++ include/System/SimulationEngine.h
@@ -1,56 +1,63 @@
 #pragma once
 #include <vector>
-#include <queue>
 #include <memory>
 #include <thread>
 #include <atomic>
-#include <mutex>
+#include <random>
 
-// Include your base classes
-#include "../Core/Machine.h"
-#include "../Core/Worker.h"
+#include "ThreadSafeQueue.h"
 #include "../Core/Batch.h"
+#include "../Harvest/FarmPlot.h"
+#include "../Harvest/Harvester.h"
+#include "../Reception/ReceptionScale.h"
+#include "../Cleaning/FlotationTank.h"
+#include "../Cleaning/DebrisRemover.h"
+#include "../Processing/PulpingMachine.h"
+#include "../Milling/OpticalSorter.h"
+#include "../Roasting/Roaster.h"
+#include "../Packaging/PackagingMachine.h"
+#include "../Warehouse/Warehouse.h"
 
 namespace FactorySim {
-
     class SimulationEngine {
     private:
-        // Thread safety
         std::atomic<bool> is_running;
-        std::mutex queue_mutex;
+        float dt_hours;
 
-        // The Master Clock
-        float current_time_hours;
-        float dt_hours; // E.g. 0.1 hours per tick
+        // Factory Entities
+        std::vector<std::unique_ptr<FarmPlot>> farms;
+        std::vector<std::unique_ptr<Harvester>> harvesters;
+        std::unique_ptr<ReceptionScale> scale;
+        std::unique_ptr<FlotationTank> tank;
+        std::unique_ptr<DebrisRemover> debris_remover;
+        std::unique_ptr<PulpingMachine> pulper;
+        std::unique_ptr<OpticalSorter> sorter;
+        std::unique_ptr<Roaster> roaster;
+        std::unique_ptr<PackagingMachine> packager;
+        std::unique_ptr<Warehouse> warehouse;
 
-        // Entity Lists (Allows growing/shrinking the factory dynamically)
-        std::vector<std::unique_ptr<Machine>> factory_machines;
-        std::vector<std::unique_ptr<Worker>> factory_workers;
-
-        // The Factory Queues (Conveyor Belts)
-        std::queue<std::unique_ptr<Batch>> farm_to_reception;
-        std::queue<std::unique_ptr<Batch>> reception_to_cleaning;
-        std::queue<std::unique_ptr<Batch>> cleaning_to_processing;
-        std::queue<std::unique_ptr<Batch>> processing_to_milling;
-        std::queue<std::unique_ptr<Batch>> milling_to_roasting;
-        std::queue<std::unique_ptr<Batch>> roasting_to_packaging;
-        std::queue<std::unique_ptr<Batch>> packaging_to_warehouse;
-        std::queue<std::unique_ptr<Batch>> warehouse_to_sales;
-        std::queue<std::unique_ptr<Batch>> sales_to_delivery;
+        // Conveyor Belts (Queues)
+        ThreadSafeQueue<std::unique_ptr<Batch>> queue_reception;
+        ThreadSafeQueue<std::unique_ptr<Batch>> queue_cleaning;
+        ThreadSafeQueue<std::unique_ptr<Batch>> queue_processing;
+        ThreadSafeQueue<std::unique_ptr<Batch>> queue_milling;
+        ThreadSafeQueue<std::unique_ptr<Batch>> queue_roasting;
+        ThreadSafeQueue<std::unique_ptr<Batch>> queue_packaging;
 
     public:
         SimulationEngine();
-
-        // Setup
         void initializeFactory();
-
-        // The Master Loop
         void run();
         void stop();
 
-        // Pipeline Stages
-        void runReceptionStage();
-        void runCleaningStage();
+    private:
+        // Thread loops
+        void stageHarvesting();
+        void stageReception();
+        void stageCleaning();
+        void stageProcessing();
+        void stageMilling();
+        void stageRoasting();
+        void stagePackaging();
     };
-
 }
